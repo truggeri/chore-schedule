cdca22f77bcffb1cce7bc4af88642f90108be2aa
Thomas Ruggeri <truggeri@gmail.com>
2019-04-14 12:54:09 -0400

    Adding homepage with style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Chore Schedule"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
