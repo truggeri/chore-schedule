64dbdc80a876852ee20f8f6455a9c84e21f0ce4c
Thomas Ruggeri <truggeri@gmail.com>
2019-04-14 12:08:45 -0400

    Adding rubocop

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,9 @@
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
+ENV['RAILS_ENV'] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+module ActiveSupport
+  class TestCase
+    fixtures :none
+  end
 end
