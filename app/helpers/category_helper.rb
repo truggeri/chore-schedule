b3cf6db3f59a0fe7bae980cc78741b49fe0c50cf
Thomas Ruggeri <truggeri@gmail.com>
2019-04-19 07:53:33 -0400

    Simple Category index page

--- app/helpers/category_helper.rb
+++ app/helpers/category_helper.rb
@@ -1,2 +1,6 @@
 module CategoryHelper
+  def category_color_badge(category)
+    style = "background-color: \##{category.color}; margin-right: 8px;"
+    content_tag(:div, " ", class: "badge", style: style) if category.color.present?
+  end
 end
