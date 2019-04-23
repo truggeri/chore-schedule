6a4bb9199e1c8c9a03f1958b1ca701c3027250a0
Thomas Ruggeri <truggeri@gmail.com>
2019-04-22 22:48:01 -0400

    Cleaning up Category sidebar

--- app/helpers/chores_helper.rb
+++ app/helpers/chores_helper.rb
@@ -36,16 +36,6 @@ module ChoresHelper
     select(:chore, :category_id, options)
   end
 
-  def chore_box(chore = nil, last_row: false)
-    return content_tag(:div, "", class: "chore-box") if chore.nil?
-    contents = [content_tag(:h4, chore.description)]
-    contents << content_tag(:div, chore.last_performed_string, class: "last-done")
-    top_row = content_tag(:div, safe_join(contents), class: "top-row")
-    bottom_row_text = "Next due on #{chore&.perform_next_string}, #{days_until_due(chore, false)} days"
-    bottom_row = content_tag(:div, bottom_row_text, class: "bottom-row")
-    content_tag(:div, safe_join([top_row, bottom_row]), class: "chore-box #{'last-row' if last_row}")
-  end
-
   def days_until_due(chore, stylize = true)
     days = chore.days_until_due
     return days if days.positive?
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,19 @@
 module StaticPagesHelper
+  def category_box(category, last_row: false)
+    return nil if category.nil?
+    name_link = link_to(category.name, category_path(category))
+    header = content_tag(:h5, name_link, class: "category-header")
+    count_box = content_tag(:div, category.chore_count, class: "category-count")
+    content_tag(:div, safe_join([header, count_box]), class: "category-box")
+  end
+
+  def chore_box(chore = nil, last_row: false)
+    return content_tag(:div, "", class: "chore-box") if chore.nil?
+    contents = [content_tag(:h4, chore.description)]
+    contents << content_tag(:div, chore.last_performed_string, class: "last-done")
+    top_row = content_tag(:div, safe_join(contents), class: "top-row")
+    bottom_row_text = "Next due on #{chore&.perform_next_string}, #{days_until_due(chore, false)} days"
+    bottom_row = content_tag(:div, bottom_row_text, class: "bottom-row")
+    content_tag(:div, safe_join([top_row, bottom_row]), class: "chore-box #{'last-row' if last_row}")
+  end
 end
