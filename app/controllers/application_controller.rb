bf04b059ee45978f0f212cbfda57466f5488c50e
Thomas Ruggeri <truggeri@gmail.com>
2019-05-19 15:49:18 -0400

    Adding family indicator to header nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_family_name, if: -> { account_signed_in? }
+
   private
+
+  def set_family_name
+    session[:family_name] = current_account&.family[:name] if session[:family_name].blank?
+  end
 
   def ajax_redirect_to(url)
     head(302, x_ajax_redirect_url: url)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,12 @@ module ApplicationHelper
     content_tag(:i, "", class: "#{fa_type} fa-#{klass}")
   end
 
+  def family_display
+    return "" if session[:family_name].blank?
+    output_text = safe_join([fa_icon("users", "fas"), " #{session[:family_name]} Family"])
+    content_tag(:span, output_text, class: "navbar-family-name")
+  end
+
   def full_title(page_title = '')
     base_title = "Chore Schedule"
     if page_title.empty?
