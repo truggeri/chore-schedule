ff2de9c714e6a980ac7f015c70af98ca9cdb9db4
Thomas Ruggeri <truggeri@gmail.com>
2019-06-15 17:15:10 -0400

    Checking category link count in #index integration test

--- test/integration/categories_test.rb
+++ test/integration/categories_test.rb
@@ -4,17 +4,19 @@ class CategoriesTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   def setup
-    @family = create(:family)
+    @family   = create(:family)
     @category = create(:category, family: @family)
-
-    @account = create(:account, family: @family)
+    @account  = create(:account, family: @family)
     sign_in(@account)
   end
 
   test "/category" do
+    (0..Random.rand(0..2)).each { |_i| create(:category, family: @family) }
+
     get "/category"
     assert_response(:success)
     assert_match("<a href=\"#{category_path(@category)}\">#{@category.name}</a>", @response.body)
+    assert_equal(Category.family(@family).count, @response.body.scan(/<a href=.{1,2}\/category\/\d+/).size)
   end
 
   test "/category/:id" do
