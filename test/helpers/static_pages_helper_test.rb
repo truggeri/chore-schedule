66db062b17e281ea50e4177c4c040dd779ca15b7
Thomas Ruggeri <truggeri@gmail.com>
2019-06-16 09:17:07 -0400

    Adding test for StaticPageHelper

---
+++ test/helpers/static_pages_helper_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class StaticPagesHelperTest < ActionView::TestCase
+  include ApplicationHelper
+
+  def setup; end
+
+  test "@more_link no additional_text" do
+    expected_div = "<div class='more-link'><a href='/foo'>More <i class='fa fa-angle-double-right'></i></a></div>"
+    assert_dom_equal(expected_div, more_link("/foo"))
+  end
+
+  test "@more_link with additional_text" do
+    expected_div = "<div class='more-link'><a href='/foobar'>More foo <i class='fa fa-angle-double-right'></i></a></div>"
+    assert_dom_equal(expected_div, more_link("/foobar", "foo"))
+  end
+end
