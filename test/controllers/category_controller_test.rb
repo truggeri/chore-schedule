b821bb5e472b90e468dab37cb5f06c9872712ba7
Thomas Ruggeri <truggeri@gmail.com>
2019-07-08 00:32:59 -0400

    Removing unneeded test files

--- test/controllers/category_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CategoryControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/chore_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ChoreControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
