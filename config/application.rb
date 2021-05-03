ccebb0f4a1127ce3e59f6e37dfd59e77c15fc976
Thomas Ruggeri <truggeri@gmail.com>
2021-05-03 09:40:17 -0400

    Fixing Zeitwerk issue with Devise and ActionMailer

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative "boot"
 
 require "active_record/railtie"
 require "action_controller/railtie"
+require "action_mailer/railtie"
 require "action_view/railtie"
 require "rails/test_unit/railtie"
 require "sprockets/railtie"
